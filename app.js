fc50c427ebb9b90b937057be629aeaeb5ecc5d02
Robbie <Leon.Wh1te@proton.me>
2024-01-28 20:19:11 +0000

    missing arrow functionality`

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 
 //declare global variables
 let thumbNailBox = document.getElementById(`thumbnail-box`)
+//set displayImage directly to img tag
 let displayImage = document.getElementById(`display`)
+//set announcer
+const announcer = document.getElementById(`announcer`)
 //make images object array
 let images = [
     {
         url: `https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a4c6a31d-c586-4ac6-9172-1d5c2b9d41cf/d60wfgx-49772b1b-8b09-4a01-84eb-f04d8dd81f43.png/v1/fill/w_999,h_800,q_70,strp/south_park__wallpaper_eric_cartman_by_hieifireblaze_d60wfgx-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODIwIiwicGF0aCI6IlwvZlwvYTRjNmEzMWQtYzU4Ni00YWM2LTkxNzItMWQ1YzJiOWQ0MWNmXC9kNjB3Zmd4LTQ5NzcyYjFiLThiMDktNGEwMS04NGViLWYwNGQ4ZGQ4MWY0My5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.oX3t6Ps7YpCVd7OZDtwYyaIqTYocSNSLYGp6dJaIPKY` ,
-        alt: `cartman with a red background`
+        alt: `cartman with a red background` 
     },
     {
         url: `https://wallpapers.com/images/hd/south-park-s-stan-marsh-simple-art-ad3wjf4su96o4mam.jpg`,
@@ -18,38 +21,85 @@ let images = [
     },
     {
         url:`https://www.thefactsite.com/wp-content/uploads/2020/04/kenny-facts.jpg`, 
-        alt: `Kenny with an oramge background`
+        alt: `Kenny with an orange background`
     }
 ]
 
 //create thumbnails
-const makeThumbNails = (imagesArray) => 
+const makeThumbNails = (images) => 
 {
     //loop through array of images
-    images.forEach( image => 
+    images.forEach( (image) => 
     {
-    //create image tag using javascript
-    let imgElement = document.createElement(`img`)
+    //create image tag using javascript , save to variable
+    let img = document.createElement(`img`)
     //link src and alt
-    imgElement.src = images.url
-    imgElement.alt = images.alt
-    //append to thumbnailbox
-    thumbNailBox.appendChild(imgElement)
+    img.src = image.url
+    img.alt = image.alt
+    //create class for img tag
+    img.className= `img`
+    // event listener
+    img.addEventListener("click",() =>
+    {
+        console.log(`image.alt`)
+        //call createDisplayImage
+        createDisplayImage(image)
+      
+    })
+    
+    //append to thumbNailBox
+    thumbNailBox.appendChild(img)
+    //announce alt text 
+    announceAltText(images.alt)
+    //
+    // handleArrowKeyPress(img)
     })
 }       
 
 //get display image, seperate from thumbnail bar
-function createDisplayImage(image)
+function createDisplayImage(images)
 {
-    // set the inner html
-    displayImage.innerHTML = ``
-    //create image tag and save to variable
-    let largeDisplayImg = document.createElement(`img`)
-    //set src and alt to variable
-    largeDisplayImg.src = image.url
-    largeDisplayImg.alt = image.alt
-    //append to global display image variable
-    displayImage.appendChild(largeDisplayImg)
+    //set src and alt dire
+    displayImage.src = images.url
+    displayImage.alt = images.alt
+    displayImage.className = `display-img`
+    announceAltText(displayImage.alt)
+   
 }
+//handle arrow key event
+function handleArrowKeyPress(event)
+{
+    if (event.key === 'ArrowRight')
+    {
+        selectNextImage(currentImage+1);
+    }
+    else if (event.key === 'ArrowLeft')
+    {
+        selectNextImage( currentImage-1)
+    }
+}
+//selectNextImage function
+function selectNextImage(images)
+{
+    //declare currentImage variable
+        let currentImage = createDisplayImage(images[0])
+        currentImage +=1
+        if (currentImage >= images.length)
+        {
+            currentImage=createDisplayImage(images[0])
+        }
+    
+
+}
+//announce alt text function
+function announceAltText(altText)
+{
+    announcer.textContent = `New image displayed: ${altText}`
+}
+
 // call makeThumbnails to images object array
 makeThumbNails(images)
+//set display image on loading the page 
+createDisplayImage(images[0])
+
+handleArrowKeyPress(images)
